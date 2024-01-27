649587092b413f48d1520c7c62067bf7163f773c
skhapijulhossen <skhapijulhossen@gmail.com>
2024-01-27 22:19:24 +0530

    inference function integated

--- app/app.py
+++ app/app.py
@@ -1,23 +1,44 @@
+import sys
+import os
+
+current = os.path.dirname(os.path.abspath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+sys.path.append(os.path.join(parent, 'code'))
+
 import streamlit as st
+import tensorflow as tf
+import config
+import pandas as pd
+from code.inferencePipeline import inference
+
+# code to replace special character from string using regex
 
 
-def analyze_sentiment(text):
-    return "Positive" 
+def remove_special_characters(text):
+    """Remove special characters from a string"""
+    import re
+    return re.sub(r'[^a-zA-Z0-9\s]', '', text)
 
-def main():
+
+def run(model):
     st.title("Text Sentiment Analysis")
 
     # Input text area
-    user_input = st.text_area("Enter your text here:")
+    user_input = st.text_area("Enter your review here:")
+    data = pd.DataFrame(
+        {'reviewText': [remove_special_characters(user_input.lower()),]})
 
     # Analyze button
     if st.button("Analyze Sentiment"):
+        LABELS = ('Positive', 'Negetive')
         if user_input:
-            result = analyze_sentiment(user_input)
-            st.success(f"Sentiment: {result}")
+            result = inference(model, data)
+            label = LABELS[tf.argmax(result, axis=1)]
+            prob = result[0, label]
+            st.success(f"Sentiment {label}: {prob}")
         else:
             st.warning("Please enter some text before analyzing.")
 
 
-if __name__ == "__main__":
-    main()
+
