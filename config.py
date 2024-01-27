734fd55da1e491c27deec9499f012d9bbd0679cf
skhapijulhossen <skhapijulhossen@gmail.com>
2024-01-27 22:20:13 +0530

    model configs updates

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ LENGTH = 5000
 VOCAB_SIZE = 1000
 BATCH_SIZE = 64
 ARTIFACT_STORE = 'artifacts'
+MODEL_PATH = 'artifacts/model.h5'
+TOKENIZER_PATH = 'artifacts/tokenizer.pkl'
 EPOCHS = 10
 STEPS_PER_EPOCH = 100
